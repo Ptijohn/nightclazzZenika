f67d154ab4842b1a90b8c459b6613569984564f4
Bastien Diederichs <bastien.diederichs@gmail.com>
2018-02-19 21:49:16 +0100

    Modified array script to use localhost

--- 5-aggregation/docker-run.sh
+++ 5-aggregation/docker-run.sh
@@ -1,13 +1,18 @@
 #/usr/bin/env bash
 
+echo "Creating hidden files"
 touch "$(pwd)/.dbshell"
 touch "$(pwd)/.mongorc.js"
+echo "Sending them to docker"
 docker cp "$(pwd)/.dbshell" mongodb-nightclazz1:/.dbshell
 docker cp "$(pwd)/.dbshell" mongodb-nightclazz1:/.mongorc.js
+echo "Sending json files to docker"
 docker cp "$(pwd)/arrayToObject.json" mongodb-nightclazz1:/arrayToObject.json
 docker cp "$(pwd)/objectToArray.json" mongodb-nightclazz1:/objectToArray.json
-docker exec -t mongodb-nightclazz1 mongoimport --host 'myReplica/nightclazz-mongo1:27017,nightclazz-mongo2:27018' --drop -d test -c arrayToObject /arrayToObject.json
-docker exec -t mongodb-nightclazz1 mongoimport --host 'myReplica/nightclazz-mongo1:27017,nightclazz-mongo2:27018' --drop -d test -c objectToArray /objectToArray.json
+echo "Importing the files into MongoDB"
+docker exec -t mongodb-nightclazz1 mongoimport --host 'myReplica/localhost:27017,nightclazz-mongo2:27018' --drop -d test -c arrayToObject /arrayToObject.json
+docker exec -t mongodb-nightclazz1 mongoimport --host 'myReplica/localhost:27017,nightclazz-mongo2:27018' --drop -d test -c objectToArray /objectToArray.json
+echo "Done"
 
 
 
